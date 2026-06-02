f7484ed8a890892dc4951f9960ae34c42288ff1e
LuisFSForti <133523565+LuisFSForti@users.noreply.github.com>
2026-06-01 21:04:34 -0300

    Valores de testes para os gráficos

--- SQL/01-CREATE-TABLES.sql
+++ SQL/01-CREATE-TABLES.sql
@@ -10,3 +10,14 @@ create table if not exists EMPRESA_TESTE (
 	constraint PK_Nome_Empresa primary key(nome),
 	constraint FK_Tipo_Empresa foreign key(tipo) references tipos_empresa_teste(tipo)
 );
+
+create table if not exists RESERVAS_MES (
+	nomeEmpresa varchar(30),
+	mes varchar(3),
+	nroReservas int not null,
+	constraint PK_Reservas_Mes primary key (nomeEmpresa, mes),
+	constraint FK_Reservas_Mes_Nome_Empresa foreign key(nomeEmpresa) references EMPRESA_TESTE(nome),
+	constraint CK_Reservas_Mes_Mes_Real check(mes in ('JAN', 'FEV', 'MAR', 'ABR', 'MAI', 'JUN', 'JUL',
+														'AGO', 'SET', 'OUT', 'NOV', 'DEZ')),
+	constraint CK_Nro_Reservas_Positivo check(nroReservas > 0)
+)
--- SQL/02-INSERTS-TESTE.sql
+++ SQL/02-INSERTS-TESTE.sql
@@ -4,12 +4,31 @@ insert into tipos_empresa_teste values ('Parque aquático');
 insert into tipos_empresa_teste values ('Pousada');
 insert into tipos_empresa_teste values ('Salão de festas');
 
-select * from tipos_empresa_teste;
-
 insert into empresa_teste values ('Casagrande Olímpia Hotel', 'XBdajKIO', 'Hotel');
 insert into empresa_teste values ('Pousada Jardim Botânico', 'BJInfsjdn123', 'Pousada');
 insert into empresa_teste values ('Pousada Vivenda', 'sfd743LK', 'Pousada');
 insert into empresa_teste values ('Thermas dos Laranjais', '32498FDI9243', 'Parque aquático');
 insert into empresa_teste values ('Hot Beach Olímpia', 'kljjgdfUJBH', 'Parque aquático');
 
-select * from empresa_teste;
+insert into reservas_mes values ('Casagrande Olímpia Hotel', 'JAN', 340);
+insert into reservas_mes values ('Casagrande Olímpia Hotel', 'FEV', 300);
+insert into reservas_mes values ('Casagrande Olímpia Hotel', 'MAR', 120);
+insert into reservas_mes values ('Casagrande Olímpia Hotel', 'ABR', 80);
+insert into reservas_mes values ('Casagrande Olímpia Hotel', 'MAI', 140);
+insert into reservas_mes values ('Casagrande Olímpia Hotel', 'JUN', 200);
+
+insert into reservas_mes values ('Pousada Jardim Botânico', 'JAN', 300);
+insert into reservas_mes values ('Pousada Jardim Botânico', 'FEV', 260);
+insert into reservas_mes values ('Pousada Jardim Botânico', 'MAR', 60);
+insert into reservas_mes values ('Pousada Jardim Botânico', 'ABR', 70);
+insert into reservas_mes values ('Pousada Jardim Botânico', 'MAI', 100);
+insert into reservas_mes values ('Pousada Jardim Botânico', 'JUN', 160);
+
+insert into reservas_mes values ('Thermas dos Laranjais', 'JAN', 780);
+insert into reservas_mes values ('Thermas dos Laranjais', 'FEV', 640);
+insert into reservas_mes values ('Thermas dos Laranjais', 'MAR', 200);
+insert into reservas_mes values ('Thermas dos Laranjais', 'ABR', 180);
+insert into reservas_mes values ('Thermas dos Laranjais', 'MAI', 100);
+insert into reservas_mes values ('Thermas dos Laranjais', 'JUN', 130);
+
+select * from reservas_mes;
